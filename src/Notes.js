6c95ba55911e20c54375d8c6d7d36ebed5f38805
zeektenka <zeektenka@gmail.com>
2021-10-22 16:38:30 +0200

    2 files modified

--- src/Notes.js
+++ src/Notes.js
@@ -3,10 +3,15 @@ import React from 'react';
 function Notes() {
   return (
     <div className="notes-container">
-      <h1 className='notes-title'>What is Lorem Ipsum?</h1>
-      <p className='notes-paragraph'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <div className="note-item">
+        <h3 className="notes-title">What is Lorem Ipsum?</h3>
+        <p className="notes-paragraph">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem ....
+        </p>
 
-      <p className='notes-tags'>Tags,Tags,tags</p>
+        <p className="notes-tags">Tags,Tags,tags</p>
+      </div>
     </div>
   );
 }
